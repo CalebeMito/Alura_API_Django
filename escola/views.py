37aa98684fa052cdd1a8c0984165dea6db71dfb8
Calebe Birer <calebeocanha53@gmail.com>
2023-12-12 17:20:35 -0300

    att

--- escola/views.py
+++ escola/views.py
@@ -2,8 +2,7 @@ from django.http import JsonResponse
 
 def alunos(request):
     if request.method == 'GET':
-        # Retorna todos os dados dos alunos cadastrados no banco de dados
         aluno = {'id': 1, 'nome':'Guilherme' }
-        return JsonResponse(aluno)
+        return JsonResponse(aluno)# Retorna todos os dados dos alunos cadastrados no banco de dados
 
 
